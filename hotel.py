02a62d3ea076126321c0bcaf83db5cd3c659076e
Helen <71827720+helen-13@users.noreply.github.com>
2021-12-16 12:17:45 -0300

    Resources
    Resources usados.

--- hotel.py
+++ hotel.py
@@ -1,25 +1,54 @@
-from sql_alchemy import banco
-class HotelModel(banco.model):
-    __tablename__='hoteis'
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
-    hotel_id = banco.Column(banco.String, primary_key=True)
-    nome = banco.Column(banco.String(80))
-    estrelas = banco.Column(banco.Float(precision=1))
-    diaria= banco.Column(banco.Float(precison=2))
-    cidade = banco.Column(banco.String(40))
+hoteis: list[dict[str, str | float] | dict[str, str | float] | dict[str, str | float]] = [
+    dict(hotel_id='alpha', nome="Alpha Hotel", estrela=4.4, diaria=420.34, cidade='Rio de Janeiro'),
+    dict(hotel_id='bravo', nome="Bravo Hotel", estrela=4.3, diaria=480.34, cidade='Santa Catarina'),
+    dict(hotel_id='charlie', nome="Charlie Hotel", estrela=3.9, diaria=320.34, cidade='Santa Catarina')
+]
 
-    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
-        self.hotel_id = hotel_id
-        self.nome = nome
-        self.estrelas = estrelas
-        self.diaria = diaria
-        self.cidade = cidade
+class Hoteis(Resource):
+    def get(self):
+        return hoteis
 
-    def json(self):
-        return {
-            'hotel_id': self.hotel_id,
-            'nome': self.nome,
-            'estrelas': self.estrelas,
-            'diaria': self.diaria,
-            'cidade': self.cidade
-        }
+class Hotel(Resource):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return False
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'}, 404  # not found
+
+    def post(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def put(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        hoteis.append(novo_hotel)
+        return novo_hotel,201 #created
+
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message':'Hotel deleted.'}
